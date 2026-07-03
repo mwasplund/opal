4ded75cf93ddd26944038a43354a83ae259e5ea5
mwasplund <mwasplund@outlook.com>
2026-07-02 22:58:02 -0700

    Create path set

---
+++ source/utilities/path-set.h
@@ -0,0 +1,45 @@
+﻿// <copyright file="path-set.h" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+#pragma once
+
+namespace Opal
+{
+	/// <summary>
+	/// A set of paths
+	/// </summary>
+	export class PathSet
+	{
+	private:
+		struct DirectoryEntry
+		{
+			std::string Value;
+			std::vector<DirectoryEntry> Children;
+		};
+
+	public:
+		static PathSet BuildSet(const std::vector<Path>& set)
+		{
+			
+		}
+
+	private:
+		std::vector<DirectoryEntry> _root;
+
+	public:
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PathSet"/> class.
+		/// </summary>
+		PathSet() :
+			_root()
+		{
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether the path is empty
+		/// </summary>
+		void Serialize(std::ostream& stream) const
+		{
+		}
+	};
+}
